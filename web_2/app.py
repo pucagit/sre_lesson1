f28e6ad4e3e3440cd01549e5ce1f878bbff5e434
Phuc <hanoi2003.a@gmail.com>
2025-09-06 10:22:59 +0700

    Read IP from X-Forwarded-For header because request.remote_addr isn’t populated with the real client IP when your app sits behind Nginx → Gunicorn

--- web_2/app.py
+++ web_2/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, request, render_template, redirect, url_for # type: ignore
+from flask import Flask, request, render_template # type: ignore
 
 app = Flask(__name__)
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return redirect(url_for('index'))
+def client_ip():
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+    return ip
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    ip = request.remote_addr
+    ip = client_ip()
     method = request.method
     url = request.url
     return render_template('index.html',  ip=ip, method=method, url=url)
